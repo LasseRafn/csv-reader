76c5273f48d27f4bba93197b52d0c7ad11fa8814
Lasse Rafn <lasserafn@gmail.com>
2018-05-11 13:34:01 +0200

    Init

---
+++ tests/UniqueHeaderTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class UniqueHeaderTest extends \PHPUnit\Framework\TestCase
+{
+	/** @test */
+	public function can_trim_empty_columns_to_avoid_exceptions_with_ununique_headers() {
+		echo '';
+	}
+}
