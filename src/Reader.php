ac9a5d19a6bc2c31427f83f1ddcae51df2a74e60
Lasse Rafn <lasserafn@gmail.com>
2018-10-08 16:55:50 +0200

    New exception

--- src/Reader.php
+++ src/Reader.php
@@ -1,5 +1,6 @@
 <?php namespace LasseRafn\CsvReader;
 
+use LasseRafn\CsvReader\Exceptions\InvalidCSVException;
 use League\Csv\CharsetConverter;
 
 class Reader
@@ -9,11 +10,25 @@ class Reader
 
 	protected const DELIMITERS          = [ ',', '\t', ';', '|', ':' ];
 	protected const SUPPORTED_ENCODINGS = [
-		'UTF-8', 'ASCII', 'ISO-8859-1', 'ISO-8859-2',
-		'ISO-8859-3', 'ISO-8859-4', 'ISO-8859-5', 'ISO-8859-6',
-		'ISO-8859-7', 'ISO-8859-8', 'ISO-8859-9', 'ISO-8859-10',
-		'ISO-8859-13', 'ISO-8859-14', 'ISO-8859-15', 'ISO-8859-16',
-		'Windows-1251', 'Windows-1252', 'Windows-1254',
+		'UTF-8',
+		'ASCII',
+		'ISO-8859-1',
+		'ISO-8859-2',
+		'ISO-8859-3',
+		'ISO-8859-4',
+		'ISO-8859-5',
+		'ISO-8859-6',
+		'ISO-8859-7',
+		'ISO-8859-8',
+		'ISO-8859-9',
+		'ISO-8859-10',
+		'ISO-8859-13',
+		'ISO-8859-14',
+		'ISO-8859-15',
+		'ISO-8859-16',
+		'Windows-1251',
+		'Windows-1252',
+		'Windows-1254',
 	];
 
 	/**
@@ -118,6 +133,10 @@ class Reader
 	 * @return \League\Csv\Reader
 	 */
 	protected static function initReader( $document ) {
+		if ( $document === null ) {
+			throw new InvalidCSVException( 'No document specified' );
+		}
+
 		if ( $document instanceof \SplFileObject ) {
 			return \League\Csv\Reader::createFromPath( $document );
 		}
